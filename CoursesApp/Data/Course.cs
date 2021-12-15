93adb6a258d1e96450f6c1bfe509db00105aa3d8
Kamal Rathnayake <kamalrathnayake@outlook.com>
2021-12-15 09:27:17 +0530

    Components and databinding

---
+++ CoursesApp/Data/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace CoursesApp.Data
+{
+    public class Course
+    {
+        public string ReferenceId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
